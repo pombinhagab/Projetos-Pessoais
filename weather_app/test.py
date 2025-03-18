f7ac1ce63591023db5fce550d14158ca57d5f0e3
Gabriel N. <gabrielnascimento12t@gmail.com>
2025-03-18 14:51:13 -0300

    integrando api

--- weather_app/test.py
+++ weather_app/test.py
@@ -1,8 +1,32 @@
-import request
+import requests
 import json
 import datetime
+import pytz
 
 chave = '4079b0973de0b47cf143d444d6bde8a9'
-api_link = f"http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={chave}"
+cidade = 'Lisboa'
+api_link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={chave}"
 
+# chamando API com request
+r = requests.get(api_link)
 
+#converter os dados presentes
+dados = r.json()
+
+print(dados)
+print("*"*45)
+
+#cidade, pais e horas
+pais_codigo = dados['sys']['country']
+print(pais_codigo)
+
+#fuso
+zona_fuso = pytz.country_timezones[pais_codigo]
+
+#pais
+pais = pytz.country_names[pais_codigo]
+
+#data
+zona = pytz.timezone(zona_fuso[0])
+zona_horas = datetime.now(zona)
+print(zona)
